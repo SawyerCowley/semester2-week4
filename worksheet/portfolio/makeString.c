cad0a8011736daa4e8e1c12dc269b6aa88b89939
SawyerCowley <xmgj0858@leeds.ac.uk>
2026-03-08 14:17:44 +0000

    worksheet

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -8,9 +8,17 @@ Student ID:
 #include <string.h>
 
 int main( int argc, char **argv ) {
-
+    int i;
+    char buffer[50];
     // define appropriate data to hold your answer
-
+    for (i = 0; i < argc; i ++) {
+        //string conncatenate adds buffer almost like a running product
+        strcat(buffer, argv[i]);  
+        //adds on hyphen until last value
+        if (i < argc - 1) {      
+            strcat(buffer, "-");  
+        }
+    }
     // process the command-line data using appropriate string functions
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
